28cb4810738161b3a5fc2e7a799f7de46ed99d25
danny9mvp <danny9mvp@gmail.com>
2022-05-26 14:28:00 -0500

    Initial commit.

---
+++ src/com/poli/clientechat/model/Message.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poli.clientechat.model;
+
+/**
+ *
+ * @author dannymvp
+ */
+public class Message {
+    private String username;
+    private String content;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+}
